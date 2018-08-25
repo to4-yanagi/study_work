c135ce1ee8ba303dc4c7b042c6e59c0530371b3e
to4-yanagi <to4.yanagi@gmail.com>
2018-08-25 12:26:45 +0900

    追加処理実装

--- sticky-note/App.js
+++ sticky-note/App.js
@@ -1,21 +1,64 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
+// 付箋のコンポネントに一意なキーを設定するためにuuidを利用する。
+// https://www.npmjs.com/package/uuid
+import uuidv4 from 'uuid/v4'
+import { Button } from 'react-native-elements';
 import StickyNote from './StickyNote';
 
 export default class App extends React.Component {
+  state = {
+    StickyNoteList: [],
+  };
+
+  addStickyNote = () => {
+    const stickyNoteList = this.state.StickyNoteList;
+    const uuid = uuidv4();
+    stickyNoteList.push(
+      <StickyNote key={uuid} />
+    );
+    this.setState({ StickyNoteList: stickyNoteList });
+  };
+
   render() {
     return (
-      <View>
-        <Text style={styles.text}>
-          <StickyNote />
-        </Text>
-      </View>
+      // KeyboardAvoidingView内にテキスト入力が可能なコンポネントがあった場合
+      // 入力欄とソフトウェアキーボードが被らないように自動的に避ける
+      // https://facebook.github.io/react-native/docs/keyboardavoidingview
+      // ScrollViewの縦幅を超えた時に自動的にスクロールする
+      // https://facebook.github.io/react-native/docs/scrollview
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+        keyboardVerticalOffset={0}>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          {this.state.StickyNoteList}
+        </ScrollView>
+        <Button
+          title="＋"
+          onPress={this.addStickyNote}
+          buttonStyle={styles.addButton}
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    margin: 24,
-  }
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#ecf0f1',
+  },
+  scrollContainer: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  addButton: {
+    width: 50,
+    height: 50,
+    left: 20,
+    bottom: 20,
+    position: 'absolute',
+    borderRadius: 50,
+  },
 });
